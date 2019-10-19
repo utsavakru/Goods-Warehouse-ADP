58af3aee46dfeef78dc002f6b672571e4d218b9f
Utsav Akru <49266434+utsavakru@users.noreply.github.com>
2019-10-19 11:39:03 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.hp.introslide;
+
+import android.content.Context;
+
+import android.content.Intent;
+
+import android.content.SharedPreferences;
+
+import android.os.Bundle;
+
+import android.preference.PreferenceManager;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public boolean isFirstStart;
+
+    Context mcontext;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.activity_main);
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+
+            public void run() {
+
+                //  Intro App Initialize SharedPreferences
+
+                SharedPreferences getSharedPreferences = PreferenceManager
+
+                        .getDefaultSharedPreferences(getBaseContext());
+
+
+
+                //  Create a new boolean and preference and set it to true
+
+                isFirstStart = getSharedPreferences.getBoolean("firstStart", true);
+
+
+
+                //  Check either activity or app is open very first time or not and do action
+
+                if (isFirstStart) {
+
+
+
+                    //  Launch application introduction screen
+
+                    Intent i = new Intent(MainActivity.this, MyIntro.class);
+
+                    startActivity(i);
+
+                    SharedPreferences.Editor e = getSharedPreferences.edit();
+
+                    e.putBoolean("firstStart", false);
+
+                    e.apply();
+
+                }
+                else
+                {
+                    Intent i = new Intent(MainActivity.this, MyIntro.class);
+
+                    startActivity(i);
+
+                    SharedPreferences.Editor e = getSharedPreferences.edit();
+
+                    e.putBoolean("firstStart", false);
+
+                    e.apply();
+                }
+
+            }
+
+        });
+
+        t.start();
+    }
+
+}
